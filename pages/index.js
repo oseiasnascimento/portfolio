e8b55f868d7ecd300996b7f74d34585ff60a95b4
Oseias Nascimento <oseiassaboia2@gmail.com>
2021-03-22 11:17:43 -0300

    primeira pagina

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+function Home()  {
+    return (<div>
+
+        <h1>Início</h1> 
+        <Link href="/sobre">
+            <a>Acessar page Sobre</a>
+        </Link> 
+        
+        </div>)
+ }
+
+ export default Home
+//  { }
