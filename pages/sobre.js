928532c4ecbfd16a2e9ddba4fa83d4a03ad7be1e
Oseias Nascimento <oseiassaboia2@gmail.com>
2021-03-23 18:28:46 -0300

    testando Link

---
+++ pages/sobre.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+function Sobre()  {
+    return (<div>
+
+        <h1>Início</h1> 
+        <Link href="/sobre">
+            <a>Acessar page Sobre</a>
+        </Link> 
+        
+        
+        </div>)
+ }
+
+ export default Sobre
+//  { }
