a58d723e4a7d0921fd4fb27745a7002c23774c09
Oseias Nascimento <oseiassaboia2@gmail.com>
2022-01-30 13:51:21 -0300

    alterando nome

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 function Home()  {
     return (<div>
 
-        <h1>PORTIFÓLIO EM CONSTRUÇÃO</h1> 
+        <h1>PORTFÓLIO EM CONSTRUÇÃO</h1> 
         <Link href="/sobre">
             <a>Acessar page Sobre</a>
         </Link> 
