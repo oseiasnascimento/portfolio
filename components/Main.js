2e5380c1bd1b93a1b36598d0277f6a50108995b9
Oseias Saboia <oseiassaboia2@gmail.com>
2022-02-04 14:13:50 -0300

    alterando layout e favicon

--- components/Main.js
+++ components/Main.js
@@ -4,6 +4,7 @@ export default function Main() {
   return (
     <div className={styles.main}>
       <main>
+        <h2>Sobre mim</h2>
         <p>
           Em busca de conhecimento das melhores tecnologias de desenvolvimento
           mobile e web.
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,12 +4,16 @@ export default function Sidebar() {
   return (
     <>
       <aside className={styles.aside}>
-        <img src="https://avatars.githubusercontent.com/u/28720307?v=4" alt='Oseias Saboia' />
-        <strong>Oseias Saboia</strong>
+        <circle r="88" cx="90" cy="90" stroke="url(#gradient)"></circle>
+
+        <img
+          src="https://avatars.githubusercontent.com/u/28720307?v=4"
+          alt="Oseias Saboia"
+        />
+        <strong>Oseias Saboia Nascimento</strong>
         <span>NextJS, ReactJS, Node.js</span>
         <form>
           <div class="input-block">
-            <label htmlFor="github_username">github_username</label>
             <label htmlFor="github_username">DESENVOLVERDOR BACK-END.</label>
           </div>
         </form>
