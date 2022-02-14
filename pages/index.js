d78c75b17b9be92b829589688f5e2bc7921cb3c5
Oseias Saboia <oseiassaboia2@gmail.com>
2022-02-14 16:27:58 -0300

    ajustes em responsividade

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,6 @@ export default function Home() {
         <title>Oseias Saboia | Developer </title>
         <meta name="keywords" content="Oseias Saboia | Desenvolvedor" />
         <meta name="description" content="Desenvolvedor back-end e Front-end" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <div className={styles.app}>
         <Sidebar />
