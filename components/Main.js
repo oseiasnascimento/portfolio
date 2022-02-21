a788d488985089c0245aedb71bc4ea7dd76a859d
Oseias Saboia <oseiassaboia2@gmail.com>
2022-02-21 14:44:29 -0300

    adicionando assets

--- components/Main.js
+++ components/Main.js
@@ -3,59 +3,60 @@ import styles from '../styles/Main.module.css'
 export default function Main() {
   return (
     <div>
-      <main >
-        <div className={styles.container}>
-        <h2>Sobre mim</h2>
-        <p>
-          Sou programador há pouco mais de 3 anos e tenho experiência em
-          desenvolvimeto web,
-        </p>
-        <p>atuando como desenvolvedor júnior em Back-end/Font-end.</p>
-        <p>
-          Em busca de conhecimento das melhores tecnologias de desenvolvimento
-          web e mobile.
-        </p>
-        <div className={styles.gridsociais}>
-          <a className={styles.redesociais}
-            href="https://github.com/oseiassaboia"
-            target="_blank"
-            rel="noopener"
-            class="sc-dTGQxN dqYSOI"
-          >
-            <img src="/github.svg" alt="logo github" />
-            <span>GitHub</span>
-          </a>
-          <a
-            className={styles.redesociais}
-            href="https://www.linkedin.com/in/oseias-saboia-99ab1ba5/"
-            target="_blank"
-            rel="noopener"
-          >
-            <img src="/linkedIn.svg" alt="logo linkedIn" />
-            <span>LinkedIn</span>
-          </a>
-          <a
-            className={styles.redesociais}
-            href="https://www.instagram.com/oseiassaboia/"
-            target="_blank"
-            rel="noopener"
-          >
-            <img src="/instagram.svg" alt="logo Instagram" />
-            <span>Instagram</span>
-          </a>
-        </div>
-        </div>
-        <div className={styles.container}>
-        <h2>Tecnologias</h2>
-          <div className={styles.tech} >
+      <main>
+        <section className={styles.container}>
+          <h2>Sobre mim</h2>
+          <p>
+            Sou programador há pouco mais de 3 anos e tenho experiência em
+            desenvolvimeto web,
+          </p>
+          <p>atuando como desenvolvedor júnior em Back-end/Font-end.</p>
+          <p>
+            Em busca de conhecimento das melhores tecnologias de desenvolvimento
+            web e mobile.
+          </p>
+          <div className={styles.gridsociais}>
+            <a
+              className={styles.redesociais}
+              href="https://github.com/oseiassaboia"
+              target="_blank"
+              rel="noopener"
+              class="sc-dTGQxN dqYSOI"
+            >
+              <img src="/github.svg" alt="logo github" />
+              <span>GitHub</span>
+            </a>
+            <a
+              className={styles.redesociais}
+              href="https://www.linkedin.com/in/oseias-saboia-99ab1ba5/"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src="/linkedIn.svg" alt="logo linkedIn" />
+              <span>LinkedIn</span>
+            </a>
+            <a
+              className={styles.redesociais}
+              href="https://www.instagram.com/oseiassaboia/"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src="/instagram.svg" alt="logo Instagram" />
+              <span>Instagram</span>
+            </a>
+          </div>
+        </section>
+        <section className={styles.container}>
+          <h2>Tecnologias</h2>
+          <div className={styles.tech}>
             <div className={styles.techposition}>
               <span>Tenho experiência</span>
-              <ul>              
+              <ul>
                 <li name="Visual Basic" className={styles.techVisualbasic}></li>
-                <li name="ASP .NET" className={styles.techAspnet} ></li>
-                <li name="SQL Server" className={styles.techSqlserver} ></li>
-                <li name="CSS3" className={styles.techCss3} ></li>
-                <li name="HTML5" className={styles.techHtml5} ></li>
+                <li name="ASP .NET" className={styles.techAspnet}></li>
+                <li name="SQL Server" className={styles.techSqlserver}></li>
+                <li name="CSS3" className={styles.techCss3}></li>
+                <li name="HTML5" className={styles.techHtml5}></li>
               </ul>
             </div>
             <div className={styles.techposition}>
@@ -65,18 +66,59 @@ export default function Main() {
                 <li name="JavaScript" className={styles.techJavascript}></li>
                 <li name="NextJS" className={styles.techNextjs}></li>
                 <li name="ReactJS" className={styles.techReactjs}></li>
-            </ul>
+              </ul>
             </div>
             <div className={styles.techposition}>
               <span>Tenho interesse</span>
               <ul>
                 <li name="Flutter" className={styles.techFlutter}></li>
                 <li data-tip="C#" name="C#" className={styles.techCsharp}></li>
-                <li data-tip="TypeScript" name="TypeScript" className={styles.techTypescript}></li>
+                <li
+                  data-tip="TypeScript"
+                  name="TypeScript"
+                  className={styles.techTypescript}
+                ></li>
               </ul>
             </div>
           </div>
-        </div>
+        </section>
+        <section className={styles.container}>
+          <h2>HABILIDADES</h2>
+          <label>Visual basic</label>
+          <div className={styles.statusSombra}>
+            <div className={styles.status}>
+              <label>40%</label>
+            </div>
+          </div>
+
+          <label>ASP .NET</label>
+          <div className={styles.statusSombra}>
+            <div className={styles.status2}>
+              <label>70%</label>
+            </div>
+          </div>
+
+          <label>SQL Server</label>
+          <div className={styles.statusSombra}>
+            <div className={styles.status3}>
+              <label>55%</label>
+            </div>
+          </div>
+
+          <label>CSS3</label>
+          <div className={styles.statusSombra}>
+            <div className={styles.status4}>
+              <label>60%</label>
+            </div>
+          </div>
+
+          <label>HTML5</label>
+          <div className={styles.statusSombra}>
+            <div className={styles.status2}>
+              <label>70%</label>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   )
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,45 +4,28 @@ export default function Sidebar() {
   return (
     <>
       <aside className={styles.aside}>
-
         <div className={styles.circle}>
           <span class="c-ePqEHr c-ePqEHr-dJYsls-size-lg c-ePqEHr-ikVSlxC-css">
-            <img src="https://avatars0.githubusercontent.com/u/28720307?v=4" alt="Oseias Saboia Nascimento" />
+            <img
+              src="https://avatars0.githubusercontent.com/u/28720307?v=4"
+              alt="Oseias Saboia Nascimento"
+            />
           </span>
         </div>
-        <h2>Oseias Saboia Nascimento</h2>        
+        <h2>Oseias Saboia Nascimento</h2>
         <section>
-          <label >DESENVOLVEDOR<br/> BACK-END / FRONT-END</label>
-          <label>Desenvolvedor na SEDUC  Secretaria da Educação do Maranhão<br/></label>
+          <label>
+            DESENVOLVEDOR
+            <br /> BACK-END / FRONT-END
+          </label>
+          <label>
+            Desenvolvedor na SEDUC Secretaria da Educação do Maranhão
+            <br />
+          </label>
         </section>
-        <section className={styles.section}>
-        <h4>HABILIDADES</h4>
-        <label>Visual basic</label>
-        <div className={styles.statusSombra}>
-          <div className={styles.status}><label>40%</label></div>
+        <div className={styles.footer}>
+          Desenvolvedor Full Stack: junho, 2021
         </div>
-
-        <label>ASP .NET</label>
-        <div className={styles.statusSombra}>
-          <div className={styles.status2}><label>70%</label></div>
-        </div>
-
-        <label>SQL Server</label>
-        <div className={styles.statusSombra}>
-          <div className={styles.status3}><label>55%</label></div>
-        </div>
-
-        <label>CSS3</label>
-        <div className={styles.statusSombra}>
-          <div className={styles.status4}><label>60%</label></div>
-        </div>
-
-        <label>HTML5</label>
-        <div className={styles.statusSombra}>
-          <div className={styles.status2}><label>70%</label></div>
-        </div>
-        </section>
-        <div className={styles.footer}>Desenvolvedor Full Stack: junho, 2021</div>
       </aside>
     </>
   )
